960d2448ad04ad15edd4c65fd1be0966a46579bd
RuhamaMalik <malikruhama7@gmail.com>
2026-06-17 10:48:58 +0500

    js and python variable

---
+++ class-07/app.py
@@ -0,0 +1,4 @@
+# print('hello python!')
+
+username = 'Ruhama'
+print(username)
---
+++ class-07/index.js
@@ -0,0 +1,56 @@
+// console.log(`Hello world!`)
+
+////////// Variables (box - store data)
+
+// single line comment
+
+// var , let , const
+
+// let x; // declaration
+// x = 5; // initialization
+// x=10; // reAssign
+// console.log(x);
+
+
+// var username;
+// username = `Fatima`;
+// console.log(username);
+
+//////////////////////// 
+
+// let favColour = 'Black';
+
+// let favGame = 'Roblox';
+// console.log(favGame);
+
+// console.log(favColour);
+// console.log(favColour);
+// console.log(favColour);
+
+// favColour = 'Red';
+
+// console.log(favColour);
+// console.log(favColour);
+// console.log(favColour);
+
+////////////////////////
+
+// let age = 10;
+// let email = 'abcd@gmail.com';
+
+// console.log(age, email, 'Hello');
+// console.log(agE); // case-sensitive 
+
+
+////////////////////////
+
+// const pi = 3.142;
+// pi = 67;
+
+// console.log(pi); // error
+
+
+///////////// Home work
+
+// - Flex 
+// - Media Query
