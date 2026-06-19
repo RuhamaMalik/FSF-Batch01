9fc2a47192df70cd9aedad647be7fe60b5124785
RuhamaMalik <malikruhama7@gmail.com>
2026-06-19 10:42:10 +0500

    data types, github pages deployment

---
+++ class-08/app.js
@@ -0,0 +1,39 @@
+/* 
+Data types - (
+
+1- primitive - direct value
+    > string --  text -- '' "" ``
+    > number --  5 , 1.5 
+    > boolean -- true , false
+    > undefined -- absence of value
+
+2- non-primitive (reference data type) - value by reference
+
+   > array ---  list --- []
+   > object --- dictionary --- {name,age,email}
+)
+
+*/
+
+// let x;
+// console.log(x); // undefined
+
+
+// let username = 'Ruhama'; // string - text
+// let num = 33.7; // number
+// let isActive = true; // boolean
+// let isLoggedIn = false; // boolean
+// let noValue = undefined;
+
+
+// let users = ["Ramla", "Aliza", "Mahnoor", "Tatheer", "Haniya"];
+// console.log(users);
+
+let arr = ['apple', 55, false, undefined, [1, 2, 3], { name: 'abcd' }] // 6 elements
+
+console.log(arr);
+
+// fsf-assignment1-a
+// fsf-assignment1-b
+// fsf-assignment1-c
+// fsf-assignment1-d
