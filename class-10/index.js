d6311db302a51f73dfa177832480767d97a339c0
RuhamaMalik <malikruhama7@gmail.com>
2026-06-29 10:41:29 +0500

    array methods, object prop update, comparision operator(==, ===, !=, !==)

---
+++ class-10/index.js
@@ -0,0 +1,133 @@
+// var username ='Noor';
+// var username ='Noor';
+// var username ='Noor';
+// var username ='Mahnoor';
+
+// console.log(username);
+
+
+// let name = 'Benazir';
+// let name = 'Benazir';
+// let name = 'Benazir';
+// let name = 'Benazir';
+// let name = 'Benazir';
+
+// console.log(name);
+
+
+////////////////////////////
+
+// let colours = ['red','black','blue','purple'];
+
+// console.log(colours);
+
+// colours[3] = 'indigo';
+// colours[1] = 'green';
+
+// console.log(colours[3]);
+
+/////////////////////
+
+
+/// push --- add last
+
+// colours.push('green','pink','peach');
+
+/// pop  --- remove last
+
+// colours.pop();
+// colours.pop();
+// colours.pop();
+
+/// shift ---- remove start
+
+// colours.shift();
+// colours.shift();
+
+/// unshift --- add start
+
+// colours.unshift('yellow','brown');
+
+// console.log(colours);
+
+
+////////////////// slice (copy), splice (change original array)
+
+// slice(startIndex(include), endIndex(exclude))
+
+// let fruits = ['mango', 'banana', 'lichi', 'strawberry'];
+
+// let fruit2 = fruits.slice();
+// let fruit2 = fruits.slice(1, 3); // ['banana', 'lichi']
+// let fruit2 = fruits.slice(0, 2); // ['mango', 'banana']
+
+
+//// splice(start, deleteCount, itemsToBeAdd )
+// fruits.splice(2,2,'apple','watermelon', 'grapes');
+
+// fruits.splice(2,1)
+
+// console.log(fruits);
+// console.log(fruit2);
+
+
+/////////////////////////////////////
+
+// let product = {
+//   id: 1,
+//   colour: 'black',
+//   title: 'watch',
+//   price: 500
+// }
+
+// product.price = 700;
+
+// console.log(product);
+
+
+////////////////////////////  comparison operators (compare 2 values --- boolean)
+
+// ==
+// !=
+
+// ===
+// !==
+
+
+//  >   greater than
+// <   less than
+
+// <= less than or equals to
+// >= greater than or equals to
+
+
+// console.log(5 == 5); // true
+// console.log(5 == '5'); // true
+
+// console.log(5 != 5); // false
+// console.log(5 != '5'); // false
+// console.log(5 != 4); // true
+
+
+// console.log(5 === 5); // true
+// console.log(5 === '5'); // false
+
+
+// console.log(5 !== 5); // false
+// console.log(5 !== '5'); // true
+// console.log(5 !== 4); // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
