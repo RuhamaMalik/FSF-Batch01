b112d06385da7f6dfe6530eca42cde74d3035565
RuhamaMalik <malikruhama7@gmail.com>
2026-07-01 10:44:01 +0500

    comparision operators, if else, intro(events, function), typeof, airthmatic operators

---
+++ class-11/index.js
@@ -0,0 +1,86 @@
+// console.log(5 > 6); // false
+// console.log(6 > 6); // false
+// console.log(7 > 6); // true
+// console.log('7' > 6); // true
+
+
+// console.log(5 < 6); // true
+// console.log('5' < 6); // true
+// console.log(7 < 6); // false
+
+/////// >=
+
+// console.log(5 >= 5); // true
+// console.log(7 >= 5); // true
+// console.log('7' >= 5); // true
+// console.log(4 >= 5); // false
+
+/////////////// <=
+
+// console.log(5 <= 6); // true
+// console.log(5 <= 4); // false
+
+/////////////////////// if else
+
+//  if(condition){ // true
+// }
+
+// if (false) {
+//   console.log('parathy le aao');
+// }
+
+// let isRain = false;
+
+// if(isRain){
+//   console.log('aalu k parathy le aao');
+// }
+
+// if(5 === 5){
+// console.log('5 Equals to 5');
+// }
+
+// if('5' === 5){
+// console.log('5 Equals to 5');
+// }
+
+
+// let email = 'abc@gmail.com';
+
+// if (email == 'abc@gmail.com') {
+//   console.log('signIn Successfully!');
+// }
+// else {
+//   console.log('Invalid credentials!');
+// }
+
+
+// console.log(2 + 2); // 4
+// console.log(2 - 1); // 1
+// console.log(2 * 3); // 6
+// console.log(2 ** 3); // 8 (** - exponent)
+// console.log(10 / 2); // 5
+
+// console.log(11 % 2); // 1
+
+// console.log(14 % 2 === 0);
+
+
+///////////////////////
+
+let numInput = document.getElementById('xyz');
+
+
+function checkNum (){ // declare function
+ console.log(typeof numInput.value);
+}
+
+// checkNum(); /// call function
+// checkNum(); /// call function
+// checkNum(); /// call function
+// checkNum(); /// call function
+
+// if (5 % 2 === 0) {
+//   console.log('The number is even');
+// } else {
+//   console.log('The number is odd');
+// }
