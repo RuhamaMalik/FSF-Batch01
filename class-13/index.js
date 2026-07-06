179c329b0b4636437bf18d22098fabaa0034853c
RuhamaMalik <malikruhama7@gmail.com>
2026-07-06 10:57:02 +0500

    functions

---
+++ class-13/index.js
@@ -0,0 +1,46 @@
+///////////// functions
+
+// function printName (){
+//   console.log('Hello Samra!');
+// }
+
+
+// printName();
+// printName();
+// printName();
+
+
+
+// function greet(name='Guest',x='test){ // parameters
+//   console.log(`Hello ${name}`);
+// }
+
+// greet('Fatima'); // arguments
+// greet('Ayesha');
+// greet('Halima');
+// greet('Iqra');
+// greet();
+// greet('Dua');
+// greet();
+
+////////////////////
+
+// function sum(x=0, y=0) {
+//   // console.log(x + y);
+//   console.log('test');
+//   return x + y;
+// }
+
+
+// let result = sum(6,8); // 14
+// console.log(result);
+
+// sum(3,7); // 10
+// sum(3,5); // 8
+
+
+
+
+
+
+
