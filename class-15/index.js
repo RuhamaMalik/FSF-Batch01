774927b5093ec55291de701d1d117f05605a9a78
RuhamaMalik <malikruhama7@gmail.com>
2026-07-10 10:41:22 +0500

    logical operators, percentage example

---
+++ class-15/index.js
@@ -0,0 +1,71 @@
+//////////////// logical operator (&& , ||)--- compare condition
+
+
+////////////////// ||
+
+// 2===5 ||  3>2
+
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(false || ['apple']); // ['apple']
+// console.log('false' || true); // false
+// console.log(undefined || null); // null
+// console.log("undefined" || undefined); // "undefined"
+// console.log("undefined" || true || null); // "undefined"
+// console.log(null || true || 'ruhama'); // true 
+// console.log(undefined || 'aliza' || 55); // aliza 
+
+// '', false, 0, undefined, null
+
+// true , 'a', 1, -1,-5,7,{},[]
+
+// if ([]) {
+//   console.log('hello');
+// }
+
+
+//////////////////////// &&
+
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(undefined && 'abcd'); // undefined
+// console.log(55 && 'abcd' && null); // null
+// console.log(55 && 'abcd' && {name:'noor'}); // {name:'noor'}
+// console.log(55 && 'abcd' && {name:'noor'}&& true); // true
+
+// let age = +prompt('Enter your age!');
+
+// if (age > 5     ) {
+//   console.log('Admission successfull!');
+// } else {
+//   console.log('cartoons daikhen!');
+// }
+
+
+
+///////////////////
+
+// let age = +prompt('Enter your age!');
+
+// if (age >= 18 && age < 50) {
+//   console.log('Enjoy!');
+// } else {
+//   console.log('popcorn khaen!');
+// }
+
+///////////////////////
+
+let percentange = +prompt(`What's your Percentag?`);
+
+if (percentange >= 80) {
+  console.log(`A+ Grade`);
+} else if (percentange >= 70) {
+  console.log(`A Grade`);
+} else if (percentange >= 60) {
+  console.log(`B Grade`);
+} else if (percentange >= 50) {
+  console.log(`C Grade`);
+} else {
+  console.log('Fail');
+}
+
