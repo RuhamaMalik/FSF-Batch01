024171467836841a9464a52d9e75a911d52bdb3c
RuhamaMalik <malikruhama7@gmail.com>
2026-07-15 10:37:20 +0500

    table generation example

--- class-16-17/index.js
+++ class-16-17/index.js
@@ -50,14 +50,41 @@
 //   console.log(`7 X ${i} = ${7 * i}`);
 // }
 
-let num = document.getElementById('number');
-let start = document.getElementById('start');
-let end = document.getElementById('end');
+let _num = document.getElementById('number');
+let _start = document.getElementById('start');
+let _end = document.getElementById('end');
+let box = document.getElementById('box');
+
+
+let num = Number(_num.value);
+let start = Number(_start.value);
+let end = Number(_end.value);
 
 function printTable() {
-// console.log(typeof  num.value);
-// console.log(start);
-// console.log(end.value);
+  // console.log(typeof  num.value);
+  // console.log(start);
+  // console.log(end.value);
+
+  // if(num.value===""){
+
+  if (!num || !start || !end) {
+    alert("All Inputs are required!")
+  }
+  else if (start > end) {
+    alert('please Enter correct value!')
+  }
+  else {
+    let html = ``;
+    for (let i = start; i <= end; i++) {
+      html += `<h3>${num} X ${i} = ${num * i}</h3>`;
+    }
+
+    box.innerHTML = html;
+
+  }
+
+
+
 }
 
 
